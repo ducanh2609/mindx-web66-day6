5382c9a5d841f5ba2fdff4c069c9b061cb768a6b
ducanh2609 <ducanh.nl2@gmail.com>
2023-05-06 22:13:50 +0700

    day6

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const authenticationCheck = async (req, res, next) => {
     const decoded = jwt.verify(token, '123@lol');
     const { username } = decoded
     // Check user co trong co so du lieu khong 
-    const user = await userModel.find({ username: username })
-    if (user && user.length > 0) {
+    const user = await userModel.findOne({ username: username })
+    if (user) {
         req.user = user
         next()
     } else {
@@ -29,11 +29,34 @@ app.get('/', (req, res) => {
     res.send('Home router')
 })
 
-app.post('/login', authenticationCheck, (req, res) => {
+app.post('/register', async (req, res) => {
     const { username, password } = req.body
-    const token = jwt.sign({ username: username }, '123@lol')
-    // Tra token cho client
-    res.send({ token: token })
+    // Check trung username trong db, 
+    // neu trung username thi khong cho tao user, neu khong trung thi tao user
+    // => tim user co username == req.body.username
+    // => neu ton tai thi res.send('User da ton tai')
+    // => neu khong thi create
+    const existingUser = await userModel.findOne({ username })
+    if (existingUser) {
+        res.send('User da ton tai')
+    } else {
+        const user = await userModel.create({ username, password, role: ['user'] })
+        res.send(user)
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    // Check trong db
+    const user = await userModel.findOne({ username, password })
+    // Nếu có user thì trả token, còn không thì trả lỗi
+    if (user) {
+        const token = jwt.sign({ username: username }, '123@lol')
+        // Tra token cho client
+        res.send({ token: token })
+    } else {
+        res.send('Khong tim thay user')
+    }
 })
 
 app.listen(3000)
--- routes/user.js
+++ routes/user.js
@@ -1,24 +1,30 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
-const { users, getAllUsers, userModel } = require('../models/user')
+const { userModel } = require('../models/user')
 
 const userRouter = express.Router()
 
 const authorizationCheck = (req, res, next) => {
-    const userRoles = req.user.role
+    const userRoles = req.user.role 
     // Check xem user nay co quyen lay toan bo user khong (Authorization) == check role
     if (userRoles.includes('admin')) {
         next()
     } else {
         res.send('User khong co quyen')
     }
-
 }
 
-userRouter.get('/', async (req, res) => {
+userRouter.get('/', authorizationCheck, async (req, res) => {
     const users = await userModel.find({})
     res.send(users)
 })
+
+userRouter.patch('/', async (req, res) => {
+    const {password} = req.body
+    const user = req.user // {_id, username, password}
+    // userModel.findByIdAndUpdate || userModel.findOneAndUpdate
+})
+
+userRouter.delete() // role admin
 
 userRouter.get('/me', (req, res) => {
     res.send(req.user)
